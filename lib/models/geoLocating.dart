0399039e777d7fbffe417c74769db12624538cf9
SharpGuanaco <siruvuri.kaushik@gmail.com>
2022-06-26 03:02:37 -0400

    added geocoding code file

---
+++ lib/models/geoLocating.dart
@@ -0,0 +1,8 @@
+import 'package:geocoding/geocoding.dart';
+
+void main(List<String> args) async {
+  print("hi");
+  // String address = "13511 Clear Lake Ct.";
+  // List<Location> locations = await locationFromAddress(address);
+  // print(locations);
+}
